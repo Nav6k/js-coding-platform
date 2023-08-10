2157b2626bec21bdb768475e91fffc7d936dc569
navneet.namankaur1313@gmail.com <115049676+Nav6k@users.noreply.github.com>
2023-08-10 20:24:25 +0530

    Add files via upload

---
+++ day ten js/day10/index.js
@@ -0,0 +1,27 @@
+let str ="Har\""
+console.log(str.length); //op=4
+const sentence = 'The quick brown fox jumps over the lazy dog';
+const word ='fox';
+ console.log(sentence.includes(word))//op= true
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is': 'is not'} in the sentence`); //op=The word fox is in the sentence
+
+let str2 ="Please give Rs 1000";
+let amount = str2.slice(15);
+console.log(amount);//op= 1000
+
+//let and const variable
+let intrestRate = 0.3;
+intrestRate =1;
+console.log(intrestRate);
+
+const a= 10;
+// a=90;
+console.log(a);
+
+let name = "navneet";
+let age = 30;
+let isApproved = false;
+let FirstName = undefined;
+let selectedColor = null;
+
+
